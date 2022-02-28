53736b77a624dcbc8c2e2387483763f4bd9be6b3
Guan <821143943@qq.com>
2022-02-28 21:56:19 +0800

    Setup framework

---
+++ packages/promotion/src/App.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+</script>
+
+<main />
+
+<style>
+    :root {
+        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans',
+            'Helvetica Neue', sans-serif;
+    }
+
+    main {
+        text-align: center;
+        margin: 0 auto;
+    }
+</style>
